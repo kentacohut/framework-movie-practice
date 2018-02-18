d262bfbcfca63ef39fb0b226153baa1afc3a73fe
Steve Hallam <StevenAHallam@gmail.com>
2018-02-18 11:36:55 -0800

    Basic styling and dynamically rendering static movielist data.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,11 +5,27 @@ import ReactDOM  from 'react-dom';
 class MovieList extends React.Component {
   constructor() {
     super();
+    this.state = {
+    	movies : [
+		  {title: 'Mean Girls'},
+		  {title: 'Hackers'},
+		  {title: 'The Grey'},
+		  {title: 'Sunshine'},
+		  {title: 'Ex Machina'},
+		]
+    }
   }
 
   render() {
     return (
-      <div></div>
+      <div className="movielist">
+      	<div className="header">
+      	<h1>Movie List</h1>
+      	</div>
+      	<div className="movies">
+      	{this.state.movies.map((movie, index)=><div className="movie"key={index}><p>{movie.title}</p></div>)}
+      	</div>
+      </div>
     )
   }
 }
